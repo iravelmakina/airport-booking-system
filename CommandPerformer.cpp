602a9f1c14d16685eb4913b37f15577759c00bca
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2024-09-11 12:43:06 +0300

    CommandPerformer methods
    - introduced methods to check, book,  unbook and view tickets
    - included error messages fo better user experience

---
+++ CommandPerformer.cpp
@@ -0,0 +1,88 @@
+#include "CommandPerformer.h"
+
+CommandPerformer::CommandPerformer(Registry &registry) : registry(registry) {
+}
+
+void CommandPerformer::check(const std::string &date, const std::string &flightNumber) const {
+    const Airplane *airplane = getAirplaneOrError(date, flightNumber);
+    airplane->showAvailableSeatsWithPrices();
+}
+
+
+void CommandPerformer::book(const std::string &date, const std::string &flightNumber, const std::string &seat,
+                            const std::string &username) const {
+    // it does not modify command performer
+    Airplane *airplane = getAirplaneOrError(date, flightNumber);
+    int bookedTicketId = airplane->bookSeatGetId(seat);
+    if (bookedTicketId != -1) {
+        Ticket ticket(bookedTicketId, username, date, flightNumber, seat, airplane->getSeatPrice(seat));
+        registry.addTicket(bookedTicketId, ticket);
+        registry.addUserTicket(username, bookedTicketId);
+        std::cout << "Confirmed with ID " << bookedTicketId << std::endl;
+    } else {
+        std::cerr << "Error: Seat " << seat << " is already booked." << std::endl;
+    }
+}
+
+
+void CommandPerformer::unbook(int id) const {
+    const Ticket *ticket = registry.getTicket(id);
+    if (ticket) {
+        registry.getAirplane(ticket->date, ticket->flightNumber)->unbookSeat(ticket->seat);
+        registry.removeTicket(id);
+        registry.removeUserTicket(ticket->username, ticket->id); // getters
+        std::cout << "Confirmed " << ticket->price << "$" << "refund for " << ticket->username << std::endl;
+    } else {
+        std::cerr << "Error: Ticket with ID " << id << " not found." << std::endl;
+    }
+}
+
+
+void CommandPerformer::view(const std::string &username) const {
+    std::vector<int> ticketsId = registry.getUserTickets(username);
+    if (!ticketsId.empty()) {
+        for (int idIndex = 0; idIndex < ticketsId.size(); ++idIndex) {
+            const Ticket *ticket = registry.getTicket(ticketsId[idIndex]);
+            std::cout << idIndex + 1 << ". Flight" << ticket->flightNumber << ", " << ticket->date << ", seat " <<
+                    ticket->
+                    seat << ", price " << ticket->price << "$" << std::endl;
+        }
+    } else {
+        std::cerr << "Error: No tickets found for user " << username << std::endl;
+    }
+}
+
+
+void CommandPerformer::view(int id) const {
+    const Ticket *ticket = registry.getTicket(id);
+    if (ticket) {
+        std::cout << "Flight " << ticket->flightNumber << ", " << ticket->date << ", seat " << ticket->seat << ", " <<
+                ticket->price << "$" << std::endl;
+    } else {
+        std::cerr << "Error: Ticket with ID " << id << " not found." << std::endl;
+    }
+}
+
+
+void CommandPerformer::view(const std::string &date, const std::string &flightNumber) const {
+    const Airplane *airplane = getAirplaneOrError(date, flightNumber);
+    const std::vector<int> seatsId = airplane->getBookedSeatsId();
+    if (!seatsId.empty()) {
+        for (auto const id: seatsId) { // error
+            const Ticket *ticket = registry.getTicket(id);
+            std::cout << ticket->seat << " " << ticket->username << " " << ticket->price << "$" << std::endl;
+        }
+    } else {
+        std::cerr << "Error: No seats are booked for flight " << flightNumber << " on date " << date << std::endl;
+    }
+}
+
+
+Airplane *CommandPerformer::getAirplaneOrError(const std::string &date, const std::string &flightNumber) const {
+    Airplane *airplane = registry.getAirplane(date, flightNumber);
+    if (!airplane) {
+        std::cerr << "Error: Airplane not found for date " << date << " and flight number " << flightNumber <<
+                std::endl;
+    }
+    return airplane;
+}
