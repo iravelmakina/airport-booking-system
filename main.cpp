548df4ba14625cf93995a528b679ff2df1084e5d
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2024-09-12 20:33:42 +0300

    main() and CMakeLists.txt
    - added files to cmake and implemented main() func

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
-#include "Airplane.h"
-#include "Ticket.h"
+
+#include "Registry.h"
+#include "FileReader.h"
+#include "CommandPerformer.h"
+#include "CLI.h"
 
 int main() {
-    Ticket ticket(1, "JohnDoe", "2023-10-01", "FL123", "12A", 200, false);
-    ticket.book();
+    std::unique_ptr<Registry> registry(new Registry());
+    if (!FileReader::processFile("Config.txt", *registry)) {
+        return 1;
+    }
+
+    std::unique_ptr<CommandPerformer> commandPerformer(new CommandPerformer(std::move(registry)));
+    std::cout << "Welcome to airport booking system!" << std::endl;
+    const CLI cli(std::move(commandPerformer));
+    cli.run();
     return 0;
 }
